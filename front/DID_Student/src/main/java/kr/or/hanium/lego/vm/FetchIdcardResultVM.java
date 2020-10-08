0372329a8fba2f6e2821b5466085335c4a40472d
im57 <eovhehd1986@gmail.com>
2020-10-08 13:44:23 +0900

    resolve back button err

---
+++ front/DID_Student/src/main/java/kr/or/hanium/lego/vm/FetchIdcardResultVM.java
@@ -0,0 +1,11 @@
+package kr.or.hanium.lego.vm;
+
+
+import java.time.LocalDateTime;
+
+public class FetchIdcardResultVM {
+    private String studentId;
+    private String university;
+    private LocalDateTime verifiedDate;
+    private String department;
+}
---
+++ front/DID_Student/src/main/java/kr/or/hanium/lego/vm/SendEmailVM.java
@@ -0,0 +1,6 @@
+package kr.or.hanium.lego.vm;
+
+
+public class SendEmailVM {
+    private String email;
+}
